e47fa957694e72ad86c9b0d791b7a7b90b34b1ec
Ashish <ashishka@cybage.com>
2015-03-09 10:28:21 +0530

    Done with changes

--- ecomm_project/Navigation.php
+++ ecomm_project/Navigation.php
@@ -5,24 +5,6 @@
 
 
 <a class="semi-transparent-button" href="add_product.php"> ADD </a><br/>
-<!--
-	<table>
-			<caption> <font color="Green"><b>Order By</b></font></caption>
-		<tr>
-			<td>
-				<input type="radio" name="order" value="ASC"/><font color="green">Asc</font><br/>
-				<input type="radio" name="order" value="DESC"/><font color="green">Desc</font>
-				<select id="order" name="order">
-					<option value="Select" default>--Select--</option>
-					<option value="price">By Price</option>
-					<option value="product_name">By Name</option>
-				</select>
-				
-			</td>
-		</tr>
-	</table>	
-						
--->
 
 
 </div>
