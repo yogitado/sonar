318e264c58b3b7ace8f3c486648b13b0292a198d
gandhark <gandhark@cybage.com>
2015-03-10 12:36:31 +0530

    gandhar: applied bootstrap on login admin page

---
+++ ecomm_project/js/adminLoginValidate.js
@@ -0,0 +1,14 @@
+// <![CDATA[
+            
+    var frmvalidator = new Validator("register");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+
+    frmvalidator.addValidation("name","req","Please provide your name");
+    
+    frmvalidator.addValidation("password","req","Please provide a password");
+
+    
+	
+	
+// ]]>
