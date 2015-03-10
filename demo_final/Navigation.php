cfc35e26d607cc20122efedecd5703d9434955f2
Priya <priyal@priyal.cybage.com>
2015-03-10 10:25:50 +0530

    Priya: commit done

---
+++ demo_final/Navigation.php
@@ -0,0 +1,13 @@
+<div id="nav">
+
+
+
+
+<a class="semi-transparent-button" href="add_product.php"> ADD </a><br/>
+
+		
+						
+
+
+
+</div>
---
+++ demo_final/js/scriptFunction.js
@@ -0,0 +1,16 @@
+function delete_product(value)
+				{
+				//alert(value);
+				var delete_rec="delete_rec";
+				var url="Product.php?del="+value+"&operation="+delete_rec;
+				var url1="Product.php?delete="+value;
+				//alert(url);
+				window.location=url;
+				}
+				
+				
+				function update_product(value)
+				{
+				//alert(value);
+				window.location="fetch_query.php?update="+value;
+				}
