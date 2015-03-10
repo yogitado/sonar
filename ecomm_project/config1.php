8cda3ad97e6a2e76087178e5c8117ce4cf31cabc
dipika <dipikaj@cybage.com>
2015-03-10 11:09:41 +0530

    Dipika : change in DB config file for central DB

--- ecomm_project/config1.php
+++ ecomm_project/config1.php
@@ -2,9 +2,9 @@
 // Declaring config class constants
 class Config
  {
-    const HOST     = 'localhost',
-          USER     = 'root',
-          PASSWORD = '',
+    const HOST     = '172.27.59.54',
+          USER     = 'ecomm',
+          PASSWORD = 'ecomm@123',
 		
 			HELPER = "Helper.php";
 		 //LOCATION ='http://localhost:81/sample/emp.php';
