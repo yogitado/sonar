bdb051db077c104a74ecd06834599c8fb7c79219
Pradeep Borse <pradeepbo@cybage.com>
2015-03-13 16:22:13 +0530

    User Activation Functionallity Added

--- ecomm_project/js/user_details_js.js
+++ ecomm_project/js/user_details_js.js
@@ -3,3 +3,9 @@ function update_user(value)
 				
 				window.location="update_query.php?update="+value;
 				}
+				
+function update1_user(value)
+				{
+				
+				window.location="update1_query.php?update="+value;
+				}
