2fed832993229d310806ad1fb592f26fd4a74abd
Pradeep Borse <pradeepbo@cybage.com>
2015-03-13 16:27:14 +0530

    New JS File Added

--- ecomm_project/js/user_details_js.js
+++ ecomm_project/js/user_details_js.js
@@ -1,5 +1,4 @@
 function update_user(value)
-<<<<<<< HEAD
 				{
 				
 				window.location="update_query.php?update="+value;
@@ -10,8 +9,3 @@ function update1_user(value)
 				
 				window.location="update1_query.php?update="+value;
 				}
-=======
-	{
-		window.location="update_query.php?update="+value;
-	}
->>>>>>> 572b21983f18fa5df3bd9857ae17a874c0dfbbcd
