7662c073b96c6533e6ee4b46852cc40c63321116
Jithin Raphi <jithinr@cybage.com>
2015-03-13 11:20:46 +0530

    Modified adminindex.php,fetch_query.php

--- ecomm_project/fetch_query.php
+++ ecomm_project/fetch_query.php
@@ -1,7 +1,5 @@
-
-    <?php include "Helper.php"; 
+<?php include "Helper.php"; 
 $product_id=$_REQUEST['update'];
-
 ?>	
 <?php include "html/adminHeader.html";?>
 	
@@ -10,31 +8,27 @@ $product_id=$_REQUEST['update'];
 <form style="align:center" action="Product.php" enctype="multipart/form-data" method="post">
 <div>
 <pre>
- <?php
+<?php
  $product_id=$_REQUEST['update'];
- $result=$helper->read_record("*","product_details","product_id='$product_id'"); // For all records
-				if(is_array($result))
-				{
-					foreach($result as $row)
-					{
-						
+ $result=$helper->read_record("*","product_details","product_id='$product_id'");
+ if(is_array($result)){
+ foreach($result as $row){
 ?>
-					<input type="hidden" name="product_id" id="pn"  value="<?php echo $row['product_id'];?>" /></br>
+<input type="hidden" name="product_id" id="pn"  value="<?php echo $row['product_id'];?>" /></br>
 <b>Product Name: </b><input type="text" name="product_name" id="pn"  value="<?php echo $row['product_name'];?>" /></br>
 <b>Price:  </b>      <input type="text" name="price" id="price"  value="<?php  echo $row['price']; ?>" /></br>
 <b>Image Name: </b>  <input type="text" name="image_name" id="in"  value="<?php echo  $row['image_name']; ?>" /></br>
 <b>Image Path: </b>  <input type="file" name="image_path"  value="<?php echo $row['image_path']; ?>">
 <!--<b>Description : </b><textarea name="description" rows="10" cols="25" value="<?php echo $row['description']; ?>"></textarea></br>-->
 <b>Description: </b> <textarea rows="10" cols="25"name="description" value="<?php echo $row['description']; ?>"></textarea></br>
-	<?php
-		}
-	}
-	?>
+<?php
+}
+}
+?>
 <input type="submit" value="UPDATE" /> <input type="hidden" name="operation" value="update"/> <button type="button" onclick="newDoc()">Cancel</button> <input type="reset" value="Reset">
 </pre>
 </div>
 </form>
-
 <?php 
 include "html/adminNavigation.html";?>
 
