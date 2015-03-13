a544c5f3063bc1b7895b766ea1579f28d7f2c056
Jithin Raphi <jithinr@cybage.com>
2015-03-13 16:33:09 +0530

    Modified GenericClass.php

--- ecomm_project/GenericClass.php
+++ ecomm_project/GenericClass.php
@@ -9,18 +9,18 @@
     $table="user";
     $condition=" email_id='$email' AND password='$pass' ";
     $result=$obj->read_record($field, $table, $condition);
-    if (is_array($result)){
+    if (is_array($result)) {
     foreach ($result as $row){
     $_SESSION['user'] = $row['user_id'];
     $_SESSION['email'] =$row['email_id'];
     $_SESSION['pass'] =$row['password'];
-    if (isset($_SESSION['key'])){
+    if (isset($_SESSION['key'])) {
 	header("Location: OrderSummaryPageIncluded.php");
-    } else{
+    } else {
     header("Location: index.php");
     }	
     }
-    } else{
+    } else {
    echo "Invalid Login Credentials.";
    header("Location: LoginPageIncluded.php");
    }  
@@ -28,24 +28,22 @@
    public function checkFunctionSummery($email, $pass)
   {
   $obj=new Helper("ecomm"); 
-  if(isset($email) && isset($pass)){
+  if(isset($email) && isset($pass)) {
   $field="user_id";
   $table="user";
   $condition=" email_id='$email' AND password='$pass' ";
   $result=$obj->read_record($field, $table, $condition);
-  if (is_array($result)){		
-  foreach($result as $row){
-  if($_SESSION['user'] == $row['user_id']){
+  if (is_array($result)) {		
+  foreach ($result as $row) {
+  if ($_SESSION['user'] == $row['user_id']) {
   header("Location: OrderSummaryPageIncluded.php");
   }
   }	
-  }
-  else{
+  } else {
   echo "Invalid Login Credentials.";
   header("Location: LoginPageIncluded.php");
   }
-  }
-  else{
+  } else {
   echo "Invalid Login Credentials.";
   header("Location: LoginPageIncluded.php");
   }
