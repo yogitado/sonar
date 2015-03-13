601d56e1889190858c7da2d0bfdec0b446cc81d2
Jithin Raphi <jithinr@cybage.com>
2015-03-13 11:12:16 +0530

    Modified HelperPage.php,ProductViewPage.php

---
+++ ecomm_project/HelperPage.php
@@ -0,0 +1,28 @@
+<?php
+include('Helper.php');
+class HelperPage extends Helper{
+function __construct($databasename){
+		 parent::__construct($databasename);
+	}
+	
+function read_page($field,$table ,$condition){
+		$sql="SELECT $field FROM $table $condition";
+	 
+		$result=$this->con->query($sql) or die($this->con->error);
+		if($result->num_rows==0){
+			return "No rows found";
+			}
+			else{
+				while($row=$result->fetch_array(MYSQL_ASSOC)){
+					$data[]=$row;
+					}
+					return $data;
+				}
+	}
+	function __destruct(){
+		 parent::__destruct();
+	}
+}
+$helper_page=new HelperPage("ecomm");
+
+?>
