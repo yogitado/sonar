b4731fc192a51153a9dcd449ef031c2923a258d1
Ashish <ashishka@cybage.com>
2015-03-13 19:09:54 +0530

    Merge Conflict Resolved,

--- ecomm_project/GenericClass.php
+++ ecomm_project/GenericClass.php
@@ -1,110 +1,115 @@
 <?php
-   session_start();
-   class GenericClass
-   {
-   public function checFunLogin($email, $pass)
-   {
-    $obj=new Helper("ecomm");
-    $field="user_id,email_id,password";
-    $table="user";
-    $condition=" email_id='$email' AND password='$pass' ";
-<<<<<<< HEAD
-    $result=$obj->read_record($field,$table ,$condition);
-    if (is_array($result))
-    {
-    foreach($result as $row)
-    {
-		$_SESSION['user'] = $row['user_id'];
-		$_SESSION['email'] =$row['email_id'];
-		$_SESSION['pass'] =$row['password'];
-		if(isset($_SESSION['key']))					
-		{
-			header( "Location: OrderSummaryPageIncluded.php");
-		}
-		else
-		{
-			header( "Location: index.php");
-		}	
-    }
-    }
-    else
-	{
-		echo "Invalid Login Credentials.";
-		header("Location: LoginPageIncluded.php");
-	}  
-   }
-	public function checkFunctionSummery($email,$pass)
-	{
-	$obj=new Helper("ecomm"); 
-	if(isset($email) && isset($pass))
-		{
-		$field="user_id";
+
+session_start();
+
+
+
+
+class GenericClass{
+	 public function checFunLogin($email,$pass){
+       
+
+  
+		$obj=new Helper("ecomm");
+		$field="user_id,email_id,password";
 		$table="user";
+
 		$condition=" email_id='$email' AND password='$pass' ";
 		$result=$obj->read_record($field,$table ,$condition);
-		if (is_array($result))	
-		{		
-		foreach($result as $row)
-		{
-		if($_SESSION['user'] == $row['user_id'])
-		{
-			header("Location: OrderSummaryPageIncluded.php");
+
+
+ 
+		if (is_array($result))		{
+	
+		foreach($result as $row)		{
+			$_SESSION['user'] = $row['user_id'];
+	
+			$_SESSION['email'] =$row['email_id'];
+			$_SESSION['pass'] =$row['password'];
+	
+
+	
+			if(isset($_SESSION['key']))					{
+				header( "Location: OrderSummaryPageIncluded.php");
+			}
+			else{
+				header( "Location: index.php");
+			}
+	
+	
+	
 		}
-		}	
+
 		}
-		else
-		{
+		else{
+	
 			echo "Invalid Login Credentials.";
-			header( "Location: LoginPageIncluded.php");
+			
+			
+			header("Location: LoginPageIncluded.php");
 		}
-	}
-	else
-	{
-		echo "Invalid Login Credentials.";
-		header( "Location: LoginPageIncluded.php");
-	}
-=======
-    $result=$obj->read_record($field, $table, $condition);
-    if (is_array($result)) {
-    foreach ($result as $row){
-    $_SESSION['user'] = $row['user_id'];
-    $_SESSION['email'] =$row['email_id'];
-    $_SESSION['pass'] =$row['password'];
-    if (isset($_SESSION['key'])) {
-	header("Location: OrderSummaryPageIncluded.php");
-    } else {
-    header("Location: index.php");
-    }	
+
+
+
+
+
+
+	   
     }
-    } else {
-   echo "Invalid Login Credentials.";
-   header("Location: LoginPageIncluded.php");
-   }  
-   }
-   public function checkFunctionSummery($email, $pass)
-  {
-  $obj=new Helper("ecomm"); 
-  if(isset($email) && isset($pass)) {
-  $field="user_id";
-  $table="user";
-  $condition=" email_id='$email' AND password='$pass' ";
-  $result=$obj->read_record($field, $table, $condition);
-  if (is_array($result)) {		
-  foreach ($result as $row) {
-  if ($_SESSION['user'] == $row['user_id']) {
-  header("Location: OrderSummaryPageIncluded.php");
-  }
-  }	
-  } else {
-  echo "Invalid Login Credentials.";
-  header("Location: LoginPageIncluded.php");
-  }
-  } else {
-  echo "Invalid Login Credentials.";
-  header("Location: LoginPageIncluded.php");
-  }
-  }
->>>>>>> 9414fe3f485dd065c324503d19c1612c62f960fe
-  }
+	
+	
+	
+	
+	
+	public function checkFunctionSummery($email,$pass){
+			
+		$obj=new Helper("ecomm"); 
+		if(isset($email) && isset($pass)){
+
+	
+		
+		$field="user_id";
+		$table="user";
+
+		$condition=" email_id='$email' AND password='$pass' ";
+		$result=$obj->read_record($field,$table ,$condition);
+			
+				if (is_array($result))	{
+		
+		
+		
+							foreach($result as $row){
+								if($_SESSION['user'] == $row['user_id']){
+									header("Location: OrderSummaryPageIncluded.php");
+								}
+	
+							}	
+	
+	
+				}
+				else{
+					//If the login credentials doesn't match, he will be shown with an error message.
+					echo "Invalid Login Credentials.";
+					header( "Location: LoginPageIncluded.php");
+				}
+
+
+			}
+
+			else{
+						//If the login credentials doesn't match, he will be shown with an error message.
+						echo "Invalid Login Credentials.";
+						header( "Location: LoginPageIncluded.php");
+			}
+	
+	
+	
+	}
+
+
 }
+
+
+
+
 ?>
