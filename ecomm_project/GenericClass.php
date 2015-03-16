d827dad773ab6996a161fef0e057f225b513a26c
Jithin Raphi <jithinr@cybage.com>
2015-03-16 14:49:13 +0530

    Modified GenericClass.php

--- ecomm_project/GenericClass.php
+++ ecomm_project/GenericClass.php
@@ -1,115 +1,53 @@
 <?php
-
 session_start();
-
-
-
-
-class GenericClass{
-	 public function checFunLogin($email,$pass){
-       
-
-  
+class GenericClass
+{
+	 public function checFunLogin($email, $pass)
+	 {
 		$obj=new Helper("ecomm");
-		$field="user_id,email_id,password";
+		$field="user_id, email_id, password";
 		$table="user";
-
 		$condition=" email_id='$email' AND password='$pass' AND is_active='1' ";
-		$result=$obj->read_record($field,$table ,$condition);
-
-
- 
-		if (is_array($result))		{
-	
-		foreach($result as $row)		{
+		$result=$obj->read_record($field, $table, $condition);
+		if (is_array($result)) {
+		foreach ($result as $row) {
 			$_SESSION['user'] = $row['user_id'];
-	
 			$_SESSION['email'] =$row['email_id'];
 			$_SESSION['pass'] =$row['password'];
-	
-
-	
-			if(isset($_SESSION['key']))					{
-				header( "Location: OrderSummaryPageIncluded.php");
+			if (isset($_SESSION['key'])) {
+			header("Location: OrderSummaryPageIncluded.php");
+			} else {
+				header("Location: index.php");
 			}
-			else{
-				header( "Location: index.php");
-			}
-	
-	
-	
 		}
-
-		}
-		else{
-	
+		} else {
 			echo "Invalid Login Credentials.";
-			
-			
 			header("Location: LoginPageIncluded.php");
 		}
-
-
-
-
-
-
-	   
     }
-	
-	
-	
-	
-	
-	public function checkFunctionSummery($email,$pass){
-			
-		$obj=new Helper("ecomm"); 
-		if(isset($email) && isset($pass)){
-
-	
-		
+	public function checkFunctionSummery($email, $pass){
+		$obj=new Helper("ecomm");
+		if (isset($email) && isset($pass)) {
 		$field="user_id";
 		$table="user";
-
 		$condition=" email_id='$email' AND password='$pass' ";
-		$result=$obj->read_record($field,$table ,$condition);
-			
-				if (is_array($result))	{
-		
-		
-		
-							foreach($result as $row){
-								if($_SESSION['user'] == $row['user_id']){
+		$result=$obj->read_record($field, $table, $condition);
+				if (is_array($result)) {
+							foreach ($result as $row) {
+								if ($_SESSION['user'] == $row['user_id']) {
 									header("Location: OrderSummaryPageIncluded.php");
-								}
-	
-							}	
-	
-	
-				}
-				else{
-					//If the login credentials doesn't match, he will be shown with an error message.
-					echo "Invalid Login Credentials.";
-					header( "Location: LoginPageIncluded.php");
-				}
-
-
-			}
-
-			else{
-						//If the login credentials doesn't match, he will be shown with an error message.
-						echo "Invalid Login Credentials.";
-						header( "Location: LoginPageIncluded.php");
-			}
-	
-	
-	
-	}
-
-
 }
-
-
-
-
+}
+} else {
+//If the login credentials doesn't match, he will be shown with an error message.
+echo "Invalid Login Credentials.";
+header("Location: LoginPageIncluded.php");
+}
+}else {
+//If the login credentials doesn't match, he will be shown with an error message.
+echo "Invalid Login Credentials.";
+header("Location: LoginPageIncluded.php");
+}
+}
+}
 ?>
