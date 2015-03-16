18a7026e9f812d71bbb2dfaf97a3be6285344e20
kuldeepd <kuldeepd@cybage.com>
2015-03-16 17:25:34 +0530

    modified bodyhome.php v1.0

--- ecomm_project/BodyHome.php
+++ ecomm_project/BodyHome.php
@@ -8,14 +8,12 @@ $helperObj=new Helper("ecomm");
 $fields="'product_id','product_name','price','image_name','image_path','description'";
 $resultSet=$helperObj->read_all("*","product_details");
 
-if(is_array($resultSet))
-{
-	echo '<form method="POST">'; 
+if (is_array($resultSet)) {
+	echo '<form method="POST">';
 	echo '<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%"> ';
 ?>
 
     <thead>
-	       
         <tr>
             <th width="5%" >No.</th>
             <th width="35%">Image</th> 
@@ -39,25 +37,25 @@ if(is_array($resultSet))
 <?php
 echo ' <tbody>';
 ?>
- 
+
 <?php
 	$count=1;
-	foreach($resultSet as $row)
-	{
+	foreach ($resultSet as $row) {
 ?>
 	<tr>
 		<td width="60"><b><?=$count++ ?></b></td>
-        <td width="30" ><img src='<?=$row["image_path"]?>' width=150px height=150px></td> <!-- columns can have both text and images -->
+        <td width="30" ><img src='<?=$row["image_path"]?>' width=150px height=150px></td>
+		<!-- columns can have both text and images -->
 		<td width="30" ><?=$row['price'] ?></td>
 		<td width="60" ><b><?=$row['product_name'] ?></b></td>
 		<td width="120" ><?=$row['description'] ?></td>
-<?php			  
+<?php
 	echo ' <td width="10"><input type="checkbox" class="items" name="cartarr[]" value="'.implode(",",$row).'"></td> ';
 	echo ' </tr> ';
 	}
 	echo '</tbody>';
 	echo ' </table> ';
 	echo '<input type="button" name="cartarr" id="submitCart" value="Add To Cart">';
-	echo '</form>';	
+	echo '</form>';
 }
 ?>
