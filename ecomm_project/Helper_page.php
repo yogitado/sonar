d9b60c49918181d590f5b0bf8f74de6d78e8966c
Priya <priyal@priyal.cybage.com>
2015-03-17 18:35:55 +0530

    API Documentation  done in Helper_page.php

--- ecomm_project/Helper_page.php
+++ ecomm_project/Helper_page.php
@@ -1,21 +1,20 @@
 <?php
+include('Helper.php');
 /**
-*This Helper_page class consist of a function required for LIMIT query in ProductViewPage.php 
+* This Helper_page class consist of a function required for LIMIT query in ProductViewPage.php 
 *
 *The constructor is used to establish connection with database
 *
 *This class is inherited from Helper class
 */
-include('Helper.php');
 class Helper_page extends Helper
 {
 function __construct($databasename)
 	{
 		parent::__construct($databasename);
 	}
-
 	/**
-	*Fetches all values from the table in database with a condition
+	* Fetches all values from the table in database with a condition
 	*
 	*@param string $field
 	*
@@ -27,9 +26,7 @@ function __construct($databasename)
 	*
 	*@return $data
 	*/
-
 function read_page($field, $table ,$condition)
-
 	{
 		$sql="SELECT $field FROM $table $condition";
 		$result=$this->con->query($sql) or die($this->con->error);
@@ -43,7 +40,7 @@ function read_page($field, $table ,$condition)
 				}
 	}
 	/**
-	*Class destruct which closes connection
+	* Class destruct which closes connection
 	*/
 	function __destruct()
 	{
@@ -51,7 +48,7 @@ function read_page($field, $table ,$condition)
 	}
 }
 /**
-*Creates an object of an Helper_page
+* Creates an object of an Helper_page
 */
 $helper_page=new Helper_page("ecomm");
 ?>
