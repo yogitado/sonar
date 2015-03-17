f260f7f4f3c8b1c8daf201348c62a5498256d5d9
Jithin Raphi <jithinr@cybage.com>
2015-03-17 20:44:11 +0530

    Modified BodySignup.php

--- ecomm_project/BodySignup.php
+++ ecomm_project/BodySignup.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-	include "Helper.php";
-	include "Sever_User_Email_Validation.php";
+	include_once "Helper.php";
+	include_once "Sever_User_Email_Validation.php";
 	$password=$_POST["user_password"];
-	$emailId=$_POST["user_emailid"];	
+	$emailId=$_POST["user_emailid"];
 	$obj=new Helper("ecomm");
 	$val="'$emailId', '$password'";
 	$condition1=" email_id='$emailId'";
@@ -11,32 +11,27 @@ session_start();
 	$tableName="user";
 	$obj1=new Sever_User_Email_Validation("ecomm");
 	$flag=$obj1->email_Exitence($field1, $tableName, $condition1);
-    if($flag==true)
-   {					
+    if ($flag) {			
    ?>
    <script type="text/javascript">
 	myFunction2();
 	</script>
-	<?php					
+	<?php			
 	header("Location: SignupPageincluded.php");			
-	}else{
+	} else {
 	$field="email_id,password";					
 	$obj->insert($tableName,$field,$val);
 	echo "data inserted successfully";
 	$email=	$_POST['user_emailid'];
 	$pass=	$_POST['user_password'];
-	if(isset($email) && isset($pass))
-	{						
+	if (isset($email) && isset($pass)) {						
 	$field="user_id";
 	$table="user";
     $condition=" email_id='$email' AND password='$pass' ";
 	$result=$obj->read_record($field, $table, $condition);					 
-	if (is_array($result))
-	{
-    foreach($result as $row)
-   {
-    $_SESSION['user'] =$row['user_id'];
-   {
+	if (is_array($result)) {
+    foreach ($result as $row) {
+    $_SESSION['user'] =$row['user_id']; {
 	header("Location: LoginPageIncluded.php");
 	}
     }
