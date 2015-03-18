daa5e10f9ae04b6ea35874ee9cd08b790d5e1514
Jithin Raphi <jithinr@cybage.com>
2015-03-18 11:33:24 +0530

    AddressDetails.php v2.0

--- ecomm_project/AddressDetails.php
+++ ecomm_project/AddressDetails.php
@@ -1,5 +1,5 @@
 <?php
-	include "Helper.php";
+	include_once "Helper.php";
 	session_start();
 	$name=$_POST['FullName'];
 	$mob=$_POST["Mobile"];
@@ -8,23 +8,10 @@
 		$cit=$_POST["State"];
 	$cit=$_POST["City"];
 	$pinno=$_POST["PinCode"];
-	
-	
-	/* 
-		var_dump($_POST["FullName"]);
-		var_dump($_POST["Address"]);
-			var_dump($_POST["Mobile"]);
-			var_dump($_POST["City"]);
-			var_dump($_POST["Pin"]); */
-	
 	$tableName="user_details";
 	$obj=new Helper("ecomm");
 	$val="'$mob','$addr','$cit','$pinno','$name','$user_id'";
 	$field="mobile,address,city,zip,user_name,user_id";
 	$obj->insert($tableName,$field,$val);
-
 	header("Location:OrderSummaryPageIncluded.php");
-	
-	#header('Location: successAddress.html');
-
 ?>
